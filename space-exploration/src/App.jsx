11421bc1a545358c327b502564fde08a98acbf3f
shalash23 <shalashmohamed232@outlook.com>
2023-01-10 16:15:30 +0200

    starting the navbar component

---
+++ space-exploration/src/App.jsx
@@ -0,0 +1,11 @@
+import { CssBaseline, useMediaQuery } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+import { Outlet } from "react-router-dom";
+
+export default function App() {
+  return <div>Hello World</div>;
+}
